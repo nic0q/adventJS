cf34adcbef909b7875651516871d4da3b619cf22
unknown <nicolas.farfan.ch@usach.cl>
2022-02-08 13:00:03 -0300

    Ej3 El problema de los parentesis

---
+++ 3 (El típico problema de los paréntesis).js
@@ -0,0 +1,30 @@
+const isValid = (letter) => {
+  let stack = []
+  const arr = letter.split("")
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "{" || arr[i] === "[" || arr[i] === "(") {
+      stack.push(arr[i])
+    } else {
+      switch (arr[i]) {
+        case "}":
+          if (stack[stack.length - 1] === "{" && arr[i-1] !== "{") {
+            stack.pop()
+          }
+        case ")":
+          if (stack[stack.length - 1] === "(" && arr[i-1] !== "(") {
+            stack.pop()
+          }
+        case "]":
+          if (stack[stack.length - 1] === "[" && arr[i-1] !== "[") {
+            stack.pop()
+          }
+      }
+    }
+  }
+  if(stack.length === 0){
+    return true
+  }
+  return false
+  
+}
+console.log(isValid("()"))
